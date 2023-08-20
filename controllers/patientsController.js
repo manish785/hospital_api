a13fd8bcc7c18477e42c9eb7f2c63eb401e463ac
manish785 <kumarachilish1997@gmail.com>
2023-08-20 20:16:45 +0530

    fix : made some changes in the Patient Controller

--- controllers/patientsController.js
+++ controllers/patientsController.js
@@ -7,8 +7,20 @@ module.exports.register = async function(req, res) {
        // const doctorId = req.doctor._id; // Assuming the authenticated doctor's _id is available in req.user
         
         let doctor = await Doctor.findOne({});
+        
+        let patient = await Patient.find({phone_number: req.body.phone_number});
+        
+        console.log(patient);
+        if(patient.length > 0){
+            return res.status(200).json({
+                data: {
+                    patient: patient,
+                },
+                message: 'Patient Info Already Exist In Our Database!',
+            });
+        }
 
-        let patient = await Patient.create({
+        patient = await Patient.create({
             phone_number: req.body.phone_number,
             name: req.body.name,
             doctor: doctor._id, // Associate the patient with the doctor's _id
